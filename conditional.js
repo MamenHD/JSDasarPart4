cd01f91337b8a84c48fdfe8473a4b67d8a2118b6
MamenAseli <147691861+MamenHD@users.noreply.github.com>
2024-12-13 22:51:20 +0700

    Create conditional.js

---
+++ conditional.js
@@ -0,0 +1,7 @@
+// Menggunakan conditional operator
+
+const price = 100000;
+const isMember = true;
+const discount = isMember ? 0.1 : 0;
+
+console.log(`Anda mendapatkan discount sebesar ${discount * price}`);
