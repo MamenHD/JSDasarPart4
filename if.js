3e408d8a5412ecfed827cea1e1879165e3f181d9
MamenAseli <147691861+MamenHD@users.noreply.github.com>
2024-12-13 22:50:39 +0700

    Create if.js

---
+++ if.js
@@ -0,0 +1,11 @@
+// Penggunaan If dalam JS
+
+const nilaiujian = 89;
+
+if (nilaiujian >= 90){
+    console.log('Kamu lulus, Hore!!!');
+} else if (nilaiujian <= 90) {
+    console.log('Belajar lagi deh sono');
+} else {
+    console.log('Kamu anak mana?')
+}
