0221bcf291837aba4928b1783dffb516776759e9
MamenAseli <147691861+MamenHD@users.noreply.github.com>
2024-12-13 22:54:49 +0700

    Create looping.js

---
+++ looping.js
@@ -0,0 +1,7 @@
+// Penggunaan Looping di JavaScript
+
+const person = { name: 'MamenHD', origin: 'Jakarta', birthYear: 2006 };
+
+for (const property in person) {
+  console.log(`bernilai ${person[property]}`);
+}
