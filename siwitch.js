4a7a68def4771d3bc020810122c0cc1b7858203e
MamenAseli <147691861+MamenHD@users.noreply.github.com>
2024-12-13 22:51:52 +0700

    Create siwitch.js

---
+++ siwitch.js
@@ -0,0 +1,20 @@
+// Menggunakan Switch
+
+const fruit = 'MamenHD';
+
+switch (fruit) {
+  case 'banana': // isi dari nilai
+    console.log('I am a banana.'); // perintah yang akan muncul
+    break; // memberhentikan perintah
+  case 'apple':
+    console.log('I am an apple.');
+    break;
+  case 'orange':
+    console.log('I am an orange.');
+    break;
+  case 'strawberry':
+    console.log('I am a strawberry.');
+    break;
+  default:
+    console.log('I am not a fruit. I am a programmer.');
+}
