ab01e7a8ed090063e0ff79cb1d4d069799f22cf1
MamenAseli <147691861+MamenHD@users.noreply.github.com>
2024-12-14 23:08:33 +0700

    Create do-while.js

---
+++ do-while.js
@@ -0,0 +1,11 @@
+// Menggunakan do while
+
+let i = 0;
+
+do {
+  console.log(`Angka ke-${i} adalah ${i}.`);
+  i++;
+} while (i < 5);
+
+/* Perbedaannya jika while membaca kondisi di awal, 
+terbalik dengan do while dia akan membaca mulai dari akhir kondisi.*/
