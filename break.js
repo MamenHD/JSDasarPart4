7ee14e9c33e98cc9c1099ee712df45d9dd7dbae9
MamenAseli <147691861+MamenHD@users.noreply.github.com>
2024-12-14 23:08:55 +0700

    Create break.js

---
+++ break.js
@@ -0,0 +1,9 @@
+// penggunaan break dalam java script
+
+for (let i = 0; i < 10; i++) {
+    if (i === 5) {
+      break;
+    }
+  
+    console.log(i); //Output diakan berhenti di kondisi yang di tentukan
+  }
