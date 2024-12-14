6915a364e361e3743a65fbae14534abcfda51cc0
MamenAseli <147691861+MamenHD@users.noreply.github.com>
2024-12-14 23:09:17 +0700

    Create continue.js

---
+++ continue.js
@@ -0,0 +1,8 @@
+// Penggunaan Continue di JavaScript
+
+for (let i = 0; i < 10; i++) {
+    if (i === 5) {
+        continue;
+    }
+    console.log(i); //Output akan mengabaikan kondisi if dan melanjutkan sampai kondisi for
+  }
